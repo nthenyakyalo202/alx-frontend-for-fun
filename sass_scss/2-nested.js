3cc9838296e3aec137c4f0be3da4a89aca372d32
nthenyakyalo202 <112162285+nthenyakyalo202@users.noreply.github.com>
2023-08-17 16:41:01 +0300

    delete

--- sass_scss/2-nested.js
+++
@@ -1,6 +0,0 @@
-import { fromJS } from 'immutable';
-
-export default function accessImmutableObject(object, array) {
-  const immutableObject = fromJS(object);
-  return immutableObject.getIn(array);
-}
